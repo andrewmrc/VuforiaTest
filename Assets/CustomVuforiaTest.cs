e9665acaea95e5d1651c1502d5d1742cf8f41152
DIGIVENT2\WS <breakingmaddev@gmail.com>
2017-06-09 16:53:53 +0200

    - Ora due marker riconoscono due target diversi

---
+++ Assets/CustomVuforiaTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vuforia
+{
+
+    public class CustomVuforiaTest : MonoBehaviour
+    {
+        public GameObject targetGO;
+
+
+        private void Update()
+        {
+
+            //targetGO.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+
+            /*
+            if (!targetGO.GetComponentInChildren<Renderer>().isVisible)
+            {
+                Debug.Log("Oggetto invisibile");
+            }
+            else
+            {
+                Debug.Log("Oggetto visibile");
+                targetGO.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+            }*/
+        }
+    }
+}
