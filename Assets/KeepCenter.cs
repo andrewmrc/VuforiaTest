0f4417d1e434df01d218a53667169ec2756193c9
David Fiorin <david94lozoo@gmail.com>
2017-09-11 12:28:02 +0200

    - Script KeepCenter per tenere l'object sempre al centro della camera

---
+++ Assets/KeepCenter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepCenter : MonoBehaviour
+{
+    Vector3 centerCamera = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+
+    void Start ()
+    {
+        this.transform.position = centerCamera;
+    }
+	
+	
+	void Update ()
+    {
+        this.transform.position = centerCamera;
+    }
+}
